226bc9a5c7ec5020669c3c5b09a28eac13942566
15dredr <15dredr@whs.whsat.uk>
2021-01-13 13:21:13 +0000

    First changes

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+import pygame
 
+pygame.init()
 
+DISPLAY_SURFACE = pygame.display.set_mode([800, 600])
+FPS_CLOCK = pygame.time.Clock()
+while True:
 
-
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit()
+    DISPLAY_SURFACE.fill((255, 0, 0))
+    pygame.draw.circle(DISPLAY_SURFACE, (0, 0, 0), (300, 300), 20)
+    pygame.display.update()
+    FPS_CLOCK.tick(30)
